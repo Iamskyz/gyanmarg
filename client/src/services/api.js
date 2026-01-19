2547143c9c994934a6f28532226d0ecb0563fef4
Aakash shah <154683650+Iamskyz@users.noreply.github.com>
2026-01-19 23:47:11 +0530

    base url of backend changed in services/api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gyanmarg-backend.vercel.app/api",
+  baseURL: "https://adequate-essence-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
